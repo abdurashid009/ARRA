f0b11e35e8a902c09e3a37483ea0755c4b086afc
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-22 19:06:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let fruit = ['ismoil','Aziza','Madina','Lola'];
+console.log(fruit)
+let fruit1 = alert('Boshlanishida array uzunligi 4');
+let first = confirm('Siz arrayni malumotlarini olib tashlamoiqchimisiz')
+if(first === true){
+    let fruit2 = alert('arrayning uzunligi 3');
+    
+       console.log(fruit.pop());
+       console.log(fruit)
+
+    
+}
+else{
+    let fruit3 = alert('arrayning uzunligi 4');
+    console.log(fruit)
+
+}
